0865d53dac2983b4ab60100423110042dd6bffe4
Hollis Crowder <hollis.crowder@gmail.com>
2024-02-26 11:49:38 -0800

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// create 'employee' object
+const employee = {
+    name: "Hollis",
+    streetAddress: "274A 5th Ave, San Francisco, CA 94118"
+};
+
+// non-destructively update employee object with key and value
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    return {
+            ...object,
+            [key]: value,
+        };
+};
+
+// destructively update employee object with key and value
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+};
+
+// non-destructively deletes key from employee
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+// destructively deletes key from employee
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
